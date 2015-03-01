8d7421a29acf7e226af5f22df5ee1cb9261b37e5
armi1189 <armi1189@gmail.com>
2015-03-02 00:52:17 +0100

    Exercise 7 drills

--- lrthw/ex7.rb
+++ lrthw/ex7.rb
@@ -1,7 +1,14 @@
+# Example of strings
+
 puts "Mary had a little lamb."
 puts "It's fleece was white as #{'snow'}."
 puts "And everywhere that Mary went."
+
+# Example of string multiplication
+
 puts "." * 10
+
+# Define variables
 
 end1 = "C"
 end2 = "h"
@@ -16,5 +23,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# Difference between print and puts
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
