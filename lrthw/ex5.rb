0ad6dcdb434292510c93301c557106fb3b7f7d20
armi1189 <armi1189@gmail.com>
2015-03-01 22:26:49 +0100

    Exercise 5 drills

--- lrthw/ex5.rb
+++ lrthw/ex5.rb
@@ -1,18 +1,20 @@
-my_name = 'Andrea Armiliato'
-my_age = 25
-my_height = 173 # cm
-my_weight = 72 # kg
-my_eyes = 'dark brown'
-my_teeth = 'white'
-my_hair = 'light brown'
+name = 'Andrea Armiliato'
+age = 25
+height = 173 # cm
+weight = 72 # kg
+inches = Integer(height * 0.39370)
+lbs = Integer(weight / 0.45359)
+eyes = 'dark brown'
+teeth = 'white'
+hair = 'light brown'
 
-puts "Let's talk about #{my_name}"
-puts "He's #{my_height} centimeters tall."
-puts "He's #{my_weight} kilograms heavy."
+puts "Let's talk about #{name}"
+puts "He's #{height} centimeters (or #{inches} inches) tall."
+puts "He's #{weight} kilograms (or #{lbs} pounds) heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If i add #{age}, #{height}, #{inches}, #{weight} and #{lbs} I get #{age + height + inches + weight + lbs}."
  
