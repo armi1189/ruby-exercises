1f97a5d75db3adec7e01ed27b18e8bd1843e4e1e
armi1189 <armi1189@gmail.com>
2015-03-01 22:49:08 +0100

    Exercise 6 drills

--- lrthw/ex6.rb
+++ lrthw/ex6.rb
@@ -1,21 +1,29 @@
+# Define variables
 types_of_people = 10
 x = "There are #{types_of_people} types of people."
 binary = "binary"
 do_not = "don't"
 y = "Those who know #{binary} and those who #{do_not}."
 
+# Put variables x and y
 puts x
 puts y
 
+# Put strings that includes variables x and y
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
+# Define variable hilarious with a boolean value
 hilarious = false
+# Define a variable with a string that includes the boolean variable
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+# Put variable joke_evaluation
 puts joke_evaluation
 
+# Define w and e variables
 w = "This is the left side of..."
 e = "a string with a right side."
 
+# Put variables w and e variables on on the same line
 puts w + e
