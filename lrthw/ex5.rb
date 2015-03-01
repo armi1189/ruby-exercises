ea086aac1a7517d6e05985a87107434322178d6f
armi1189 <armi1189@gmail.com>
2015-03-01 22:05:30 +0100

    Exercise 5

---
+++ lrthw/ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Andrea Armiliato'
+my_age = 25
+my_height = 173 # cm
+my_weight = 72 # kg
+my_eyes = 'dark brown'
+my_teeth = 'white'
+my_hair = 'light brown'
+
+puts "Let's talk about #{my_name}"
+puts "He's #{my_height} centimeters tall."
+puts "He's #{my_weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+ 
