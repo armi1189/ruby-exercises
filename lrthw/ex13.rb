0252bfe5184893b7db4f2d8c5749b2dcb00f7fb1
armi1189 <armi1189@gmail.com>
2015-03-02 12:57:21 +0100

    Exercise 13 drills

--- lrthw/ex13.rb
+++ lrthw/ex13.rb
@@ -5,3 +5,4 @@ puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
 
+puts "A string with all your variables: #{first}, #{second}, #{third}." 
