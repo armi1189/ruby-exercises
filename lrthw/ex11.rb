1edbd4a22d31564429cccac72515f1aa60293a04
armi1189 <armi1189@gmail.com>
2015-03-02 11:47:09 +0100

    Exercise 11 drills

--- lrthw/ex11.rb
+++ lrthw/ex11.rb
@@ -6,3 +6,14 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# Write another form like the one above
+print "What's your name? "
+name = gets.chomp
+print "Where are you from? "
+nat = gets.chomp
+print "What's your main hobby?"
+hobby = gets.chomp
+
+puts "So, you're #{name}, you are from #{nat} and your main hobby is #{hobby}."
+
