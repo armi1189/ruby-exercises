57bfe236c49366df123331f99c6be29f6fa3459a
armi1189 <armi1189@gmail.com>
2015-03-02 20:01:04 +0100

    Exercise 17 drills 2

--- lrthw/ex17.rb
+++ lrthw/ex17.rb
@@ -1,25 +1,3 @@
-from_file, to_file = ARGV
+# All the script in one line!!
 
-puts "Ok buddy, now i'm going to copy from #{from_file} to #{to_file}"
-in_file = open(from_file)
-indata = in_file.read
-
-puts "For your information, the input file is #{indata.length} bytes"
-
-if File.exist?(to_file)
-	puts "I see that the file #{to_file} already exist! Fine.. let's work.." 
-else
-	puts "Mhh.. the file #{to_file} doesn't exist.. I'll create it for you.."
-end
-
-puts "Ok we're ready.. are you sure you want to copy the file? (Hit RETURN to confirm)"
-$stdin.gets
-
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-puts "Finito! See you next time!"
-
-out_file.close
-in_file.close
-
+from_file, to_file = ARGV; puts "Ok buddy, now i'm going to copy from #{from_file} to #{to_file}"; in_file = open(from_file); indata = in_file.read; puts "For your information, the input file is #{indata.length} bytes"; if File.exist?(to_file); puts "I see that the file #{to_file} already exist! Fine.. let's work.."; else; puts "Mhh.. the file #{to_file} doesn't exist.. I'll create it for you.."; end; puts "Ok we're ready.. are you sure you want to copy the file? (Hit RETURN to confirm)"; $stdin.gets; out_file = open(to_file, 'w'); out_file.write(indata); puts "Finito! See you next time!"; out_file.close; in_file.close
